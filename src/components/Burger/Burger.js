8a6c37b4950e95d959fa78fc459ef3dd687a25ae
lununglee <leo.lunung.lee@gmail.com>
2019-09-14 12:32:04 -0700

    R08 - Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,6 +5,13 @@ import styles from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+	const transformedIngredients = Object.keys(props.ingredients)
+	.map(igKey => {
+		return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey}/>
+			})
+	})
+	console.log(transformedIngredients)
 	return (
 		<div className={styles.Burger}>
 			<BurgerIngredient type="bread-top" />
