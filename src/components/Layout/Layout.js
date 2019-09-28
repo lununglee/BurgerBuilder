669ed3afbf76c97057f972315e2368296cb4c4cd
lununglee <leo.lunung.lee@gmail.com>
2019-09-28 14:34:24 -0700

    R22 - Adding a Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import styles from './Layout.module.css'
 
 import Aux from '../../hoc/Aux'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 const layout = (props) => {
 	return (
 		<Aux>
+			<Toolbar />
 			<div>Toolbar, SideDrawer, BackDrop</div>
 			<main className={styles.Content}>
 				{props.children}
