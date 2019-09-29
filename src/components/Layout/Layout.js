50fa9c00bfbaa506164f4e9ab3bbf02c04890425
lununglee <leo.lunung.lee@gmail.com>
2019-09-28 17:17:17 -0700

    R25 - Creating a Responsive SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,12 +4,13 @@ import styles from './Layout.module.css'
 
 import Aux from '../../hoc/Aux'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => {
 	return (
 		<Aux>
 			<Toolbar />
-			<div>Toolbar, SideDrawer, BackDrop</div>
+			<SideDrawer />
 			<main className={styles.Content}>
 				{props.children}
 			</main>
