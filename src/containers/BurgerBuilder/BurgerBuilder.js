36a6d8d487b229d1ea3981de4ed4ca8055a9f6aa
lununglee <leo.lunung.lee@gmail.com>
2019-10-08 19:23:52 -0700

    R37 - Handling Errors Globally

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 const INGREDIENT_PRICES = {
 	bacon: 0.7,
@@ -127,4 +128,4 @@ class BurgerBuilder extends Component {
 	}
 }
 
-export default BurgerBuilder
+export default withErrorHandler(BurgerBuilder, Axios)
