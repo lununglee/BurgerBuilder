72374aee99efef36f53a2164c30f874228135238
lununglee <leo.lunung.lee@gmail.com>
2019-10-11 18:56:25 -0700

    R43 - Navigating Back and To Next Page

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -14,10 +14,10 @@ const checkoutSummary = (props) => {
 			</div>
 			<Button
 				btnType="Danger"
-				click>CANCEL</Button>
+				click={props.checkoutCancel}>CANCEL</Button>
 			<Button
 				btnType="Success"
-				click>CONTINUE</Button>
+				click={props.checkoutContinue}>CONTINUE</Button>
 		</div>
 	)
 }
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,18 +3,34 @@ import React, { Component } from 'react'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
 
 class Checkout extends Component {
+
 	state = {
 		ingredients: {
 			bacon: 1,
 			cheese: 1,
 			salad: 1,
 			meat: 1
-		}
+		},
+		confirmPurchase: null
 	}
+
+	checkoutCancelHandler = () => {
+		this.setState({confirmPurchase: false})
+		this.props.history.goBack()
+	}
+
+	checkoutContinueHandler = () => {
+		this.setState({confirmPurchase: true})
+		this.props.history.replace('/checkout/contact-data')
+	}
+
 	render () {
 		return (
 			<div>
-				<CheckoutSummary ingredients={this.state.ingredients}/>
+				<CheckoutSummary
+					ingredients={this.state.ingredients}
+					checkoutCancel={this.checkoutCancelHandler}
+					checkoutContinue={this.checkoutContinueHandler}/>
 			</div>
 		)
 	}
