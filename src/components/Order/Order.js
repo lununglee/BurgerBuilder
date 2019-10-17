716503abf3fec53136562855afaf4eec58d38cc2
lununglee <leo.lunung.lee@gmail.com>
2019-10-17 16:02:11 -0700

    R47 - Adding an Orders Page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './Order.module.csss'
+
+const order = () => {
+	return (
+		<div className={styles.Order}>
+			<p>Ingredients: Salad (1)</p>
+			<p>Price: <strong>USD 5.45</strong></p>
+		</div>
+	)
+}
+
+export default order
