1eaee2d14add00126b64a8d491be8176566249a6
lununglee <leo.lunung.lee@gmail.com>
2019-10-17 16:34:02 -0700

    R50 - Outputting the Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,29 @@ import React from 'react'
 
 import styles from './Order.module.css'
 
-const order = () => {
+const order = (props) => {
+	const ingredients = []
+	for (let ingredientName in props.ingredients) {
+		ingredients.push({
+			name: ingredientName,
+			amount: props.ingredients[ingredientName]
+		})
+	}
+
+	const ingredientOutput = ingredients.map(igKey => {
+		return <span style={{
+			textTransform: 'capitalize',
+			display: 'inline-block',
+			margin: '0 8px',
+			border: '1px solid #ccc',
+			padding: '5px'
+		}}>{igKey.name} ({igKey.amount})</span>
+	})
+
 	return (
-		<div className={styles.Order}>	
-			<p>Ingredients: Salad (1)</p>
-			<p>Price: <strong>USD 5.45</strong></p>
+		<div className={styles.Order}>
+			<p>Ingredients: {ingredientOutput}</p>
+			<p>Price: <strong>{props.price.toFixed(2)}</strong></p>
 		</div>
 	)
 }
