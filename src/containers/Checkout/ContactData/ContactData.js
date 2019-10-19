fc0671ab1875556b6f03cf98f05869942a7c56ea
lununglee <leo.lunung.lee@gmail.com>
2019-10-19 15:11:07 -0700

    R56 - Handling Form Submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,9 +83,14 @@ class ContactData extends Component {
 	orderHandler = (event) => {
 		event.preventDefault()
 		this.setState({loading: true})
+		const formData = {}
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+		}
 		const order = {
 			ingredients: this.props.ingredients,
-			price: this.props.price
+			price: this.props.price,
+			orderData: formData
 		}
 		Axios.post('/orders.json', order)
 			.then(response => {
@@ -112,7 +117,7 @@ class ContactData extends Component {
 			})
 		}
 		let form = (
-			<form>
+			<form onSubmit={this.orderHandler}>
 				{formElementsArray.map(formElement => (
 					<Input
 						elementType={formElement.config.elementType}
@@ -121,7 +126,7 @@ class ContactData extends Component {
 						key={formElement.id}
 						change={(event) => this.inputChangeHandler(event, formElement.id)}/>
 				))}
-				<Button btnType="Success" click={this.orderHandler}>ORDER</Button>
+				<Button btnType="Success">ORDER</Button>
 			</form>
 		)
 		if (this.state.loading) {
