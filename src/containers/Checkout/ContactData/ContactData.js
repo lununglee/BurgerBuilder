3a9aeace91975f8c2f1c1ca0cacc424b8f3179f8
lununglee <leo.lunung.lee@gmail.com>
2019-10-19 10:43:42 -0700

    R51 - Creating a Custom Dynamic Input Element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,7 @@ import styles from './ContactData.module.css'
 
 import Button from '../../../components/UI/Button/Button'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/Forms/Input/Input'
 
 class ContactData extends Component {
 	state = {
@@ -50,13 +51,13 @@ class ContactData extends Component {
 	render () {
 		let form = (
 			<form>
-					<input className={styles.Input} type="text" name="firstName" placeholder="Your First Name" />
-					<input className={styles.Input} type="text" name="lastName" placeholder="Your Last Name" />
-					<input className={styles.Input} type="text" name="email" placeholder="Your Email" />
-					<input className={styles.Input} type="text" name="street" placeholder="Your Street" />
-					<input className={styles.Input} type="text" name="city" placeholder="Your City" />
-					<input className={styles.Input} type="text" name="postal" placeholder="Your Postal Code" />
-					<input className={styles.Input} type="text" name="country" placeholder="Your Country" />
+					<Input inputtype="input" type="text" name="firstName" placeholder="Your First Name" />
+					<Input inputtype="input" type="text" name="lastName" placeholder="Your Last Name" />
+					<Input inputtype="input" type="text" name="email" placeholder="Your Email" />
+					<Input inputtype="input" type="text" name="street" placeholder="Your Street" />
+					<Input inputtype="input" type="text" name="city" placeholder="Your City" />
+					<Input inputtype="input" type="text" name="postal" placeholder="Your Postal Code" />
+					<Input inputtype="input" type="text" name="country" placeholder="Your Country" />
 				</form>
 		)
 		if (this.state.loading) {
