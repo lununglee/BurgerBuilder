06f53c8b4b6eb7fa4d83e64c29a7d81c872af539
lununglee <leo.lunung.lee@gmail.com>
2019-10-23 14:07:20 -0700

    R65 - Finishing the Reducer for Ingredients

--- src/store/reducers/ingrdntReducer.js
+++ src/store/reducers/ingrdntReducer.js
@@ -1,12 +1,30 @@
 import * as actionTypes from '../actions'
 
 const initialState = {
-	ingredients: null,
+	ingredients: {
+		bacon: 0,
+		cheese: 0,
+		salad: 0,
+		meat: 0
+	},
 	totalPrice: 4
 }
 
 const ingrdntReducer = (state = initialState, action) => {
-	return state
+	switch (action.type) {
+		case actionTypes.ADD_INGRDIENT:
+			return {
+				...state,
+				[action.ingredientName]: state.ingredients[action.ingredientName] + 1
+			}
+		case actionTypes.REMOVE_INGREDIENT:
+			return {
+				...state,
+				[action.ingredientName]: state.ingredients[action.ingredientName] - 1
+		}
+		default:
+			return state
+	}
 }
 
 export default ingrdntReducer
